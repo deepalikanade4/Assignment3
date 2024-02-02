32e90e1cd5747b639860c6fe48e10f27e75e663f
deepalikanade4 <deepalikanade4@gmail.com>
2024-02-02 14:43:39 +0530

    second commit

--- demoapp.py
+++ demoapp.py
@@ -12,16 +12,16 @@ def get_data(id=None):
     data=r.json()
     print(data)
 
-get_data(10)
+get_data(7)
 
 URL2='http://127.0.0.1:8000/createuser/'
 def put_data():
     data1={
-
-        "UserID":1011,
-        "Name": 'NEW',
-        "EmailID":'NEW@gmail.com',
-        "Designation":'Manager'
+        'id':3,
+        "UserID":1003,
+        "Name": 'ABC',
+        "EmailID":'ABC@gmail.com',
+        "Designation":'Developer'
     }
    
     json_data=json.dumps(data1)
@@ -36,10 +36,11 @@ URL3='http://127.0.0.1:8000/updateuser/'
 def update_data():
 
     data1={
-        'id':5,
-        "UserID":1003,
-        "Name": 'json',
-        "EmailID":'json@gmail.com',
+        'id':8,
+        "UserID":1012,
+        "Name": 'python',
+        "EmailID":'python@gmail.com',
+        "Destignation":'Python Developer'
     }
     json_data=json.dumps(data1)
     r=requests.put(url=URL3,data=json_data)
@@ -51,7 +52,7 @@ update_data()
 URL4='http://127.0.0.1:8000/deleteuser/'
 
 def deletedata():
-    data={'id':4}
+    data={'id':22}
     json_data=json.dumps(data)
     r=requests.delete(url=URL4,data=json_data)
     print(r.json())
--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
-for demo operation run cmd= python demoapp.py
+1.for demo operation run cmd= python demoapp.py
+2.demoapp.py file contain code  for read ,create ,update ,delete user for testing pupose
 
-can modify demoapp code(data value ) for testing pupose 
+3.can modify demoapp code(user data value ) for testing pupose 
